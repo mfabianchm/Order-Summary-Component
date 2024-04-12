2bfe9a8cc5c7be7ec0bfc371ae85921098f62fff
mfabianchm <mfchongm@hotmail.com>
2024-04-11 18:29:55 -0600

    Initial Commit

---
+++ Order-Summary-Component/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Order-Summary-Component
+//
+//  Created by Marcos Fabian Chong Megchun on 11/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
